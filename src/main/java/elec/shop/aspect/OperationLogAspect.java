1fe967f38b969886c306ea57936aa38e42f83828
zengwenqi <17784736184@163.com>
2025-08-05 14:36:03 +0800

    优化日志切面方法，提取公共方法

--- src/main/java/elec/shop/aspect/OperationLogAspect.java
+++ src/main/java/elec/shop/aspect/OperationLogAspect.java
@@ -101,110 +101,131 @@ public class OperationLogAspect {
 
             if (operationLog.isLogin()) {
                 // 处理登录日志
-                SysLoginLog loginLog = new SysLoginLog();
-                loginLog.setLogId(new SnowflakeLogIdGenerator(0L, 0L).generateId());
-                loginLog.setIp(ip);
-                loginLog.setUserId(loginSysUser.getUserId());
-                loginLog.setUsername(loginSysUser.getUsername());
-                loginLog.setTime(time);
-                loginLog.setLocation(location);
-                loginLog.setBrowser(userAgent.getBrowser().getName());
-                loginLog.setOs(userAgent.getOperatingSystem().getName());
-                loginLog.setStatus(ex == null ? 1 : 0);
-                loginLog.setMsg(ex == null ? "登录成功" : ex.getMessage());
-                loginLog.setLoginTime(new Date());
-
-                // 从登录成功的结果中获取用户信息
-                if (result instanceof Result) {
-                    Result<?> res = (Result<?>) result;
-                    if (res.getData() instanceof Map) {
-                        Map<String, Object> data = (Map<String, Object>) res.getData();
-                        loginLog.setUserId((Long) data.get("userId"));
-                        loginLog.setUsername((String) data.get("username"));
-                    }
-                }
-
-                loginLogService.save(loginLog);
+                saveLoginLog(ip, userAgent, loginSysUser, time, location, ex, result);
             } else if (operationLog.isPurchaseOrder()) {
                 // 处理采购订单日志
-                PurchaseOrderLog orderLog = new PurchaseOrderLog();
-                orderLog.setLogId(new SnowflakeLogIdGenerator(1L, 1L).generateId());
-                orderLog.setOperatorId(loginSysUser.getUserId());
-                orderLog.setOperatorName(loginSysUser.getUsername());
-                orderLog.setTime(time);
-                orderLog.setOperationType(operationLog.operationType());
-                orderLog.setOperationDesc(operationLog.description());
-                orderLog.setCreatedAt(new Date());
-
-                // 从返回结果中获取订单ID和状态
-                if (result instanceof Result) {
-                    Result<?> res = (Result<?>) result;
-                    if (res.getData() != null) {
-                        if (res.getData() instanceof Map) {
-                            Map<String, Object> data = (Map<String, Object>) res.getData();
-                            if (data.containsKey("orderId")) {
-                                orderLog.setOrderId((Long) data.get("orderId"));
-                            }
-                            if (data.containsKey("orderStatus")) {
-                                orderLog.setOrderStatus((Integer) data.get("orderStatus"));
-                            }
-                        }
-                    }
-                }
-
-                // 设置状态
-                orderLog.setTenantId(0L);
-
-                purchaseOrderLogService.save(orderLog);
+                savePurchaseOrderLog(loginSysUser,operationLog,time,result);
             } else {
                 // 处理操作日志
-                SysOperationLog sysLog = new SysOperationLog();
-                sysLog.setLogId(new SnowflakeLogIdGenerator(0L, 0L).generateId()); // 实际项目中建议使用ID生成器
-                sysLog.setUserId(loginSysUser.getUserId());
-                sysLog.setUsername(loginSysUser.getUsername());
-                sysLog.setOperationType(operationLog.operationType());
-                sysLog.setMethod(request.getMethod() + " " + request.getRequestURI());
-                sysLog.setIp(ip);
-                sysLog.setLocation(location);
-                sysLog.setBrowser(userAgent.getBrowser().getName());
-                sysLog.setOs(userAgent.getOperatingSystem().getName());
-                sysLog.setStatus(ex == null ? 1 : 0);
-                sysLog.setErrorMsg(ex != null ? ex.getMessage() : null);
-                sysLog.setTime(time);
-                sysLog.setCreatedAt(new Date());
-
-                // 处理请求参数
-                if (operationLog.saveRequestData()) {
-                    Object[] args = joinPoint.getArgs();
-                    String[] parameterNames = signature.getParameterNames();
-                    Map<String, Object> params = new HashMap<>();
-
-                    for (int i = 0; i < args.length; i++) {
-                        String paramName = parameterNames[i];
-                        Object arg = args[i];
-
-                        if (arg instanceof HttpServletRequest) {
-                            continue;
-                        }
-
-                        if (arg instanceof MultipartFile) {
-                            MultipartFile file = (MultipartFile) arg;
-                            Map<String, Object> fileInfo = new HashMap<>();
-                            fileInfo.put("fileName", file.getOriginalFilename());
-                            fileInfo.put("fileSize", file.getSize());
-                            fileInfo.put("contentType", file.getContentType());
-                            params.put(paramName, fileInfo);
-                        } else {
-                            params.put(paramName, arg);
-                        }
-                    }
-                    sysLog.setParams(JSON.toJSONString(params));
-                }
-
-                operationLogService.save(sysLog);
+                saveOperationLog(joinPoint,signature,ip,location,loginSysUser,operationLog,time,request,userAgent,ex);
             }
         } catch (Exception e) {
             log.error("日志记录异常：", e);
         }
+    }
+
+    /**
+     * 保存登录日志
+     */
+    private void saveLoginLog(String ip, UserAgent userAgent, SysUser loginSysUser, long time, String location, Exception ex, Object result){
+        SysLoginLog loginLog = new SysLoginLog();
+        loginLog.setLogId(new SnowflakeLogIdGenerator(0L, 0L).generateId());
+        loginLog.setIp(ip);
+        loginLog.setUserId(loginSysUser.getUserId());
+        loginLog.setUsername(loginSysUser.getUsername());
+        loginLog.setTime(time);
+        loginLog.setLocation(location);
+        loginLog.setBrowser(userAgent.getBrowser().getName());
+        loginLog.setOs(userAgent.getOperatingSystem().getName());
+        loginLog.setStatus(ex == null ? 1 : 0);
+        loginLog.setMsg(ex == null ? "登录成功" : ex.getMessage());
+        loginLog.setLoginTime(new Date());
+
+        // 从登录成功的结果中获取用户信息
+        if (result instanceof Result) {
+            Result<?> res = (Result<?>) result;
+            if (res.getData() instanceof Map) {
+                Map<String, Object> data = (Map<String, Object>) res.getData();
+                loginLog.setUserId((Long) data.get("userId"));
+                loginLog.setUsername((String) data.get("username"));
+            }
+        }
+
+        loginLogService.save(loginLog);
+    }
+
+    /**
+     * 保存采购订单日志
+     */
+    private void savePurchaseOrderLog(SysUser loginSysUser, OperationLog operationLog, long time, Object result){
+        PurchaseOrderLog orderLog = new PurchaseOrderLog();
+        orderLog.setLogId(new SnowflakeLogIdGenerator(1L, 1L).generateId());
+        orderLog.setOperatorId(loginSysUser.getUserId());
+        orderLog.setOperatorName(loginSysUser.getUsername());
+        orderLog.setTime(time);
+        orderLog.setOperationType(operationLog.operationType());
+        orderLog.setOperationDesc(operationLog.description());
+        orderLog.setCreatedAt(new Date());
+
+        // 从返回结果中获取订单ID和状态
+        if (result instanceof Result) {
+            Result<?> res = (Result<?>) result;
+            if (res.getData() != null) {
+                if (res.getData() instanceof Map) {
+                    Map<String, Object> data = (Map<String, Object>) res.getData();
+                    if (data.containsKey("orderId")) {
+                        orderLog.setOrderId((Long) data.get("orderId"));
+                    }
+                    if (data.containsKey("orderStatus")) {
+                        orderLog.setOrderStatus((Integer) data.get("orderStatus"));
+                    }
+                }
+            }
+        }
+
+        // 设置状态
+        orderLog.setTenantId(0L);
+
+        purchaseOrderLogService.save(orderLog);
+    }
+
+    /**
+     * 保存操作日志
+     */
+    private void saveOperationLog(ProceedingJoinPoint joinPoint,MethodSignature signature,String ip, String location,SysUser loginSysUser, OperationLog operationLog, long time, HttpServletRequest request, UserAgent userAgent, Exception ex){
+        SysOperationLog sysLog = new SysOperationLog();
+        sysLog.setLogId(new SnowflakeLogIdGenerator(0L, 0L).generateId()); // 实际项目中建议使用ID生成器
+        sysLog.setUserId(loginSysUser.getUserId());
+        sysLog.setUsername(loginSysUser.getUsername());
+        sysLog.setOperationType(operationLog.operationType());
+        sysLog.setMethod(request.getMethod() + " " + request.getRequestURI());
+        sysLog.setIp(ip);
+        sysLog.setLocation(location);
+        sysLog.setBrowser(userAgent.getBrowser().getName());
+        sysLog.setOs(userAgent.getOperatingSystem().getName());
+        sysLog.setStatus(ex == null ? 1 : 0);
+        sysLog.setErrorMsg(ex != null ? ex.getMessage() : null);
+        sysLog.setTime(time);
+        sysLog.setCreatedAt(new Date());
+
+        // 处理请求参数
+        if (operationLog.saveRequestData()) {
+            Object[] args = joinPoint.getArgs();
+            String[] parameterNames = signature.getParameterNames();
+            Map<String, Object> params = new HashMap<>();
+
+            for (int i = 0; i < args.length; i++) {
+                String paramName = parameterNames[i];
+                Object arg = args[i];
+
+                if (arg instanceof HttpServletRequest) {
+                    continue;
+                }
+
+                if (arg instanceof MultipartFile) {
+                    MultipartFile file = (MultipartFile) arg;
+                    Map<String, Object> fileInfo = new HashMap<>();
+                    fileInfo.put("fileName", file.getOriginalFilename());
+                    fileInfo.put("fileSize", file.getSize());
+                    fileInfo.put("contentType", file.getContentType());
+                    params.put(paramName, fileInfo);
+                } else {
+                    params.put(paramName, arg);
+                }
+            }
+            sysLog.setParams(JSON.toJSONString(params));
+        }
+
+        operationLogService.save(sysLog);
     }
 }
